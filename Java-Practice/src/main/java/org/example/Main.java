e222095c914adfb0b65ef1456e0b837be2bd3f1f
sidq ali <sidq.ali@gomotive.com>
2024-05-20 15:27:08 +0500

    Adding changes for learning

--- Java-Practice/src/main/java/org/example/Main.java
+++ Java-Practice/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
     }
+
 }
