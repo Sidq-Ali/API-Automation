0a7e832f9c3410b58d6632c0019354dc320f1a30
sidq ali <sidq.ali@gomotive.com>
2024-06-10 12:33:21 +0500

    API AUto

--- Java-Practice/src/main/java/OOP.java
+++
@@ -1,9 +0,0 @@
-public class OOP {
-    public static void main(String[] args) {
-        Pen pen1 = new Pen();
-        pen1.Color = "Blue";
-        pen1.Type = "Gel";
-        pen1.write();
-
-    }
-}
